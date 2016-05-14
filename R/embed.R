53ca2b8c43b74a5cbb5be64b5ec9766df7f63dfb
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-05-14 18:28:55 -0500

    adds code of conduct, readme

--- R/embed.R
+++ R/embed.R
@@ -1,25 +1,43 @@
+.convert_allowfullscreen <- function(x){
+
+  if (x){
+    result <- ""
+  } else {
+    result <- NULL
+  }
+
+  result
+}
+
 #' Creates an iframe for video-embedding
 #'
-#' @param id       character, vimeo_id
-#' @param height   numeric, height of iframe (pixels)
-#' @param width    numeric, width of iframe (pixels)
+#' @param id                character, url identifier
+#' @param height            numeric, height of iframe (px)
+#' @param width             numeric, width of iframe (px)
+#' @param frameborder       numeric, size of frame border (px)
+#' @param allow_full_screen logical, indicates if to allow fullscreen
 #'
 #' @return html code describing iframe
 #' @examples
-#' embed_vimeo(45196609)
+#' embed_vimeo("45196609")
 #'
 #' @export
 #
-embed_vimeo <- function(id, width = 500, height = 281){
+embed_vimeo <- function(id, width = 500, height = 281,
+                        frameborder = 0, allow_full_screen = TRUE){
+
+  # consider other options to send as query parameters
+
+  allowfullscreen <- .convert_allowfullscreen(allow_full_screen)
 
   htmltools::tags$iframe(
     src = paste0("https://player.vimeo.com/video/", as.character(id)),
-    width = as.character(width),
-    height = as.character(height),
-    frameborder = 0,
-    webkitallowfullscreen = "",
-    mozallowfullscreen = "",
-    allowfullscreen = ""
+    width = width,
+    height = height,
+    frameborder = frameborder,
+    webkitallowfullscreen = allowfullscreen,
+    mozallowfullscreen = allowfullscreen,
+    allowfullscreen = allowfullscreen
   )
 }
 
@@ -30,13 +48,16 @@ embed_vimeo <- function(id, width = 500, height = 281){
 #'
 #' @export
 #
-embed_youtube <- function(id, width = 420, height = 315){
+embed_youtube <- function(id, width = 420, height = 315,
+                          frameborder = 0, allow_full_screen = TRUE){
+
+  allowfullscreen <- .convert_allowfullscreen(allow_full_screen)
 
   htmltools::tags$iframe(
     src = paste0("https://www.youtube.com/embed/", as.character(id)),
-    width = as.character(width),
-    height = as.character(height),
-    frameborder = 0,
-    allowfullscreen = ""
+    width = width,
+    height = height,
+    frameborder = frameborder,
+    allowfullscreen = allowfullscreen
   )
 }
