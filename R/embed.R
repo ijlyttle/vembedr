b901cb931b7f8404b101df26068e05e21086e248
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-05-20 21:36:17 -0500

    updates function documentation

--- R/embed.R
+++ R/embed.R
@@ -1,16 +1,36 @@
 #' Creates an iframe for video-embedding
 #'
-#' @param id                character, url identifier
+#' These functions are used to embed video into your RMarkdown html documents,
+#' or into your Shiny apps. There are currently functions for you to embed from
+#' either YouTube or Vimeo.
+#'
+#' Both of these services allow you to customize a lot of things by specifying
+#' an optional query string. The specification for the query string will differ
+#' according to the service being used:
+#'
+#' \describe{
+#'   \item{YouTube}{\url{https://developers.google.com/youtube/player_parameters}}
+#'   \item{Vimeo}{\url{https://developer.vimeo.com/player/embedding}}
+#' }
+#'
+#' @param id                character, identifier provided by the service
 #' @param height            numeric, height of iframe (px)
 #' @param width             numeric, width of iframe (px)
 #' @param frameborder       numeric, size of frame border (px)
 #' @param allow_full_screen logical, indicates if to allow fullscreen
 #' @param query             list of items to include in query string
 #'
-#' @return html code describing iframe
+#' @return html \code{<iframe>} element
+#'
+#' @name embed
 #' @examples
 #' embed_vimeo("45196609")
+#' embed_youtube("dQw4w9WgXcQ")
+#' embed_youtube("8SGif63VW6E", query = list(start = secs("4m12s")))
 #'
+NULL
+
+#' @rdname embed
 #' @export
 #
 embed_vimeo <- function(id, width = 500, height = 281,
@@ -36,13 +56,7 @@ embed_vimeo <- function(id, width = 500, height = 281,
   )
 }
 
-#' Creates an iframe for video-embedding
-#'
-#' @inheritParams  embed_vimeo
-#'
-#' @examples
-#' embed_youtube("dQw4w9WgXcQ")
-#'
+#' @rdname embed
 #' @export
 #
 embed_youtube <- function(id, width = 420, height = 315,
--- R/rickroll.R
+++ R/rickroll.R
@@ -1,20 +1,28 @@
 #' Embeds a popular video
 #'
-#' @param ... arguments passed on to \code{embed_foo}
+#' If you want to experiment with the arguments to \code{\link{embed}},
+#' such as \code{query}, but do not have a particular video in mind, this function
+#' may be useful to you.
 #'
-#' @return html code describing iframe
+#' @param ... arguments (other than \code{id}) passed on to \code{\link{embed}}
 #'
+#' @return html \code{<iframe>} element
+#'
+#' @name rickroll
 #' @examples
 #'   rickroll_vimeo()
 #'   rickroll_youtube()
 #'
+NULL
+
+#' @rdname rickroll
 #' @export
 #
 rickroll_vimeo <- function(...){
   embed_vimeo(id = "45196609", ...)
 }
 
-#' @rdname rickroll_vimeo
+#' @rdname rickroll
 #' @export
 #
 rickroll_youtube <- function(...){
