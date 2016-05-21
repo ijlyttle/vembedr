06bce0ee31aadd5f1ab101fb186274cb42ef8e50
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-05-20 21:37:07 -0500

    adds package documentation

---
+++ R/vembedr.R
@@ -0,0 +1,11 @@
+#' vembedr: A package for embedding video
+#'
+#' The vembedr package provides two categories of functions:
+#' embed and rickroll
+#'
+#' @section Embed functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name vembedr
+NULL
