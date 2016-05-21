d1ec15387cb42a1f97835d4d92417686d5c40ed2
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-05-20 21:46:56 -0500

    updates package documentation, roxygenize

--- R/vembedr.R
+++ R/vembedr.R
@@ -1,10 +1,11 @@
 #' vembedr: A package for embedding video
 #'
 #' The vembedr package provides two categories of functions:
-#' embed and rickroll
+#' embed and rickroll, to embed videos from the services: YouTube and Vimeo.
 #'
-#' @section Embed functions:
-#' The foo functions ...
+#' The important functions are \code{\link{embed_vimeo}}
+#' and \code{\link{embed_youtube}}, with a useful helper function for
+#' specifying the start time of a YouTube video: \code{\link{secs}}.
 #'
 #' @docType package
 #' @name vembedr
