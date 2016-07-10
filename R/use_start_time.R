7ba49f240c5bc2ca3a6461cef1cd6455228b2ea0
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-07-10 12:35:02 -0500

    attempt to satisfy S3 checks

--- R/use_start_time.R
+++ R/use_start_time.R
@@ -62,3 +62,24 @@ use_start_time.embed_vimeo <- function(embed, start_time, ...){
 
   embed
 }
+
+#' @rdname use_start_time
+#' @export
+#'
+use_start_time.embed_channel9 <- function(embed, start_time, is_paused, ...){
+
+  # get the src from the embed
+  src <- htmltools::tagGetAttribute(embed, "src")
+
+  # parse the url
+  url <- httr::parse_url(src)
+
+  # set the time in url$fragment
+  url$fragment <- paste0("at=", secs(start_time))
+
+  # set the url in the embed
+  # == need to ask about a public API for this in htmltools ==
+  embed$attribs$src <- httr::build_url(url)
+
+  embed
+}
