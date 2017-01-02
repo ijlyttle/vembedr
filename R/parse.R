4af1e91804f658eaae703eb7d51ebd21580762c1
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-01-02 10:42:12 -0600

    gets basics working for `suggest_embed()`

--- R/parse.R
+++ R/parse.R
@@ -15,7 +15,7 @@
 parse_video_url <- function(url){
 
   list_parse <- list(
-    `channel9.msdn.com` = identity,
+    `channel9.msdn.com` = .parse_channel9,
     `www.youtube.com` = .parse_youtube,
     `youtu.be` = .parse_youtube_short,
     `vimeo.com` = .parse_vimeo
@@ -57,10 +57,52 @@ parse_video_url <- function(url){
 
 .parse_channel9 <- function(url_parsed){
 
+  path_split <-
+    url_parsed$path %>%
+    strsplit("/") %>%
+    `[[`(1)
+
+  path_user2016 <-
+    c("Events", "useR-international-R-User-conference", "useR2016")
+
+  if (identical(length(path_split), 4L)
+      && identical(path_split[1:3], path_user2016)){
+    # this is a UseR! 2016 link
+    result <- list(
+      service = "user2016",
+      id = path_split[[4]],
+      start_time = NULL
+    )
+  } else {
+    # this is a regular Channel 9 link
+    result <- list(
+      service = "channel9",
+      id = url_parsed$path,
+      start_time = NULL
+    )
+  }
+
+  result
 }
-
-
 
 suggest_embed <- function(url){
 
+  parse_list <- parse_video_url(url)
+
+  str_embed <-
+    paste0("embed_", parse_list$service, "(\"", parse_list$id, "\")")
+
+  if (is.null(parse_list$start_time)){
+    str_start_time <- NULL
+  } else {
+    str_start_time <-
+      paste0("use_start_time(\"", parse_list$start_time, "\")")
+  }
+
+  str_message <-
+    paste(c(str_embed, str_start_time), collapse = " %>%\n  ")
+
+  cat(str_message)
+
+  invisible(str_message)
 }
